3b6babad21d67d5816098213b0a12f11310406aa
Kristina Dedova <dan005bk@gmail.com>
2024-02-21 21:10:50 +0300

    Created Activity

--- app/src/main/java/com/unoth/dashboardapp/MainActivity.java
+++ app/src/main/java/com/unoth/dashboardapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.unoth.dashboardapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ContactAdapter adapter;
+    private RecyclerView recyclerViewContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        List<ContactDomain> contactsList = new ArrayList<>();
+        contactsList.add(new ContactDomain("John", "user_1"));
+        contactsList.add(new ContactDomain("Lucy", "user_2"));
+        contactsList.add(new ContactDomain("Mary", "user_3"));
+        contactsList.add(new ContactDomain("Alexa", "user_4"));
+        contactsList.add(new ContactDomain("Rose", "user_5"));
+        recyclerViewContact = findViewById(R.id.recyclerView);
+        recyclerViewContact.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        adapter = new ContactAdapter(contactsList);
+        recyclerViewContact.setAdapter(adapter);
     }
 }
